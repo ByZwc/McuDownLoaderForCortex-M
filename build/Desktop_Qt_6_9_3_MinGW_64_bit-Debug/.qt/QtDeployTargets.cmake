779eba1593a2c8ac0397f1a6906edc0b3febc59b
ByZwc <86605319+ByZwc@users.noreply.github.com>
2025-10-17 17:48:23 +0800

    Delete build directory

--- build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_McuDownLoaderForCortex-M_FILE G:/48  QTFile/McuDownLoaderForCortex-M/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/McuDownLoaderForCortex-M.exe)
-set(__QT_DEPLOY_TARGET_McuDownLoaderForCortex-M_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_McuDownLoaderForCortex-M_RUNTIME_DLLS D:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
